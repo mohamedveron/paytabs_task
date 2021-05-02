8dbe54dfccd1bbc09b94fb6bbf79585e2c2b8f73
mohamedveron <mohamedveron23@gmail.com>
2021-05-02 06:34:51 +0200

    add make transfer between accounts api

--- api/make_transfer.go
+++ api/make_transfer.go
@@ -1,7 +1,27 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func (s * Server) MakeTransfer(w http.ResponseWriter, r *http.Request) {
+
+	var transferRequest Transfer
+	if err := json.NewDecoder(r.Body).Decode(&transferRequest); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err := s.svc.MakeTransfer(transferRequest.From, transferRequest.To, transferRequest.Balance)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode("Transfer Succeeded")
 
 }
---
+++ service/make_transfer.go
@@ -0,0 +1,51 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"github.com/mohamedveron/paytabs_task/domains"
+	"strconv"
+)
+
+func (s *Service) MakeTransfer(from , to string, balance float32) error {
+
+	var fromAccount domains.Account
+	var toAccount domains.Account
+
+	if _, ok := s.AccountsDB[from]; ok {
+		fromAccount = s.AccountsDB[from]
+	}else{
+		return errors.New("wrong from account")
+	}
+
+	if _, ok := s.AccountsDB[to]; ok {
+		toAccount = s.AccountsDB[to]
+	}else{
+		return errors.New("wrong to account")
+	}
+
+
+	fromBalance, err := strconv.ParseFloat(fromAccount.Balance, 64)
+	toBalance, err := strconv.ParseFloat(toAccount.Balance, 64)
+
+	if err != nil {
+		return err
+	}
+
+	if balance > float32(fromBalance) {
+		return errors.New("don't have enough money")
+	}
+
+	toBalance+= float64(balance)
+	newToBalance := fmt.Sprintf("%f", toBalance)
+
+	fromBalance-= float64(balance)
+	newFromBalance := fmt.Sprintf("%f", fromBalance)
+
+	toAccount.Balance = newToBalance
+	fromAccount.Balance = newFromBalance
+	s.AccountsDB[to] = toAccount
+	s.AccountsDB[from] = fromAccount
+
+	return nil
+}
